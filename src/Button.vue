f6b4437425644040d45c63a0b4fe874f9435a3ba
captain-tab <d491405879@gmail.com>
2020-09-30 10:48:25 +0800

    button add postion property

--- src/Button.vue
+++ src/Button.vue
@@ -1,32 +1,64 @@
 <template>
-   <button class="s-button">
-     <slot></slot>
-   </button>
+  <button class="s-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use :xlink:href=`#s-${icon}`></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-export default {}
+export default {
+  props: ['icon', 'iconPosition']
+}
 </script>
 
 <style scoped lang="scss">
-    .s-button {
-      height: var(--button-height);
-      padding: 0 1em;
-      font: inherit;
-      color: var(--color);
-      border-radius: var(--border-radius);
-      border: 1px solid var(--border-color) ;
-      background: var(--button-bg);
-      &:hover {
-        border-color: var(--border-color);
-      }
-      &:active {
-        background-color: var(--button-active-bg);
-        box-shadow: var(--box-show);
-        color: #FFFFFF;
-      }
-      &:focus {
-        outline: none;
-      }
+.s-button {
+  height: var(--button-height);
+  padding: 0 1em;
+  font: inherit;
+  color: var(--color);
+  border-radius: var(--border-radius);
+  border: 1px solid var(--border-color);
+  background: var(--button-bg);
+  vertical-align: middle;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    border-color: var(--border-color);
+  }
+
+  &:active {
+    background-color: var(--button-active-bg);
+    box-shadow: var(--box-show);
+    color: #FFFFFF;
+  }
+
+  &:focus {
+    outline: none;
+  }
+  > .content {
+    order: 2;
+  }
+  > .icon {
+    order: 1;
+    margin-right: .3em;
+  }
+
+  &.icon-right {
+    > .content {
+      order: 1;
     }
+    > .icon {
+      order: 2;
+      margin-right: 0;
+      margin-left: .3em;
+    }
+  }
+}
 </style>
