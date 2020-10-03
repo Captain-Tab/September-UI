7e2ebb578c9321669979db7bd48de5237616744e
captain-tab <d491405879@gmail.com>
2020-10-03 15:57:54 +0800

    add test code

--- src/app.js
+++ src/app.js
@@ -13,3 +13,53 @@ new Vue({
         isLoading: false
     }
 })
+
+// 单元测试
+import chai from 'chai'
+
+const expect = chai.expect
+{
+    const Constructor = Vue.extend(Button) // 将vue组件转化为构造函数
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    }) // 通过构造函数构造新的button组件
+    button.$mount('#test')
+    let useElement = button.$el.querySelector('use') // 获取组件的use元素
+   let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#s-setting') // 期望组件的链接为s-setting
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    const Constructor = Vue.extend(Button) // 将vue组件转化为构造函数
+    const button = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    }) // 通过构造函数构造新的button组件
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    button.$mount(div)
+    let svg = button.$el.querySelector('svg') // 获取组件的svg元素
+    let { order } = window.getComputedStyle(svg)
+    expect(order).to.eq('1') // 期望组件的位置为1
+    button.$el.remove()
+    button.$destroy()
+}
+{
+    //
+    const Constructor = Vue.extend(Button) // 将vue组件转化为构造函数
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount()
+    vm.$on('click', function () {
+        console.log(1)
+    })
+    let svg = button.$el// 获取组件的svg元素
+    button.click()
+}
