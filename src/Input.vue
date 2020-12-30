e484943b17634af1ebd75c302610e808299e955a
captain-tab <d491405879@gmail.com>
2020-12-30 22:54:43 +0800

    update style of input componet

--- src/Input.vue
+++ src/Input.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="wrapper">
     <label>
-      <input type="text" :value="value">
+      <input
+          type="text"
+          :value="value"
+          :disabled="disabled"
+      >
     </label>
   </div>
 </template>
@@ -12,6 +16,10 @@ name: "s-input",
   props: {
     value: {
       type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
     }
   }
 }
@@ -27,16 +35,23 @@ $box-shadow-color: rgba(0,0,0,0.5);
 .wrapper {
   font-size: $font-size;
   display: inline-block;
-  > input {
+  > label > input {
     height: 32px;
     border: 1px solid $border-color;
     padding: 0 8px;
     font-size: inherit;
+    border-radius: $border-radius;
     &:hover {
-      border-color: $border-color-hover
+      //border-color: $border-color-hover;
     }
     &:focus {
       box-shadow: inset 0 1px 3px $box-shadow-color;
+      outline: none;
+    }
+    &[disabled] {
+      border-color: #aaa;
+      color: #aaa;
+      cursor: not-allowed;
     }
   }
 }
