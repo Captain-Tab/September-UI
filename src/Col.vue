5361f7693109fd669463c1376cca8b4f23bd7e5b
captain-tab <d491405879@gmail.com>
2021-01-05 22:52:04 +0800

    add gutter arguments

--- src/Col.vue
+++ src/Col.vue
@@ -1,14 +1,29 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col"
+       :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}"
+  >
+     <div style="border: 1px solid green; height: 100px">
+       <slot></slot>
+     </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Col",
+  data () {
+    return {
+      gutter: 0
+    }
+  },
   props: {
-    span: [Number, String]
+    span: {
+      type: [Number, String],
+    },
+    offset: {
+      type: [Number, String],
+    },
   }
 }
 </script>
@@ -16,15 +31,19 @@ export default {
 <style scoped lang="scss">
 .col {
   height: 100px;
-  background: gray;
   width: 50%;
-  border: 1px solid red;
-  flex-shrink: 0;
 
   $prefix: col-;
   @for $n from 1 through 24 {
     &.#{$prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+
+  $prefix: offset-;
+  @for $n from 1 through 24 {
+    &.#{$prefix}#{$n} {
+      margin-left: ($n/24) * 100%;;
     }
   }
 }
