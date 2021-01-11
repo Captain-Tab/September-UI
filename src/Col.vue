f374fc5c7c9688cb2d6c0374d2b1b36858cc5b0a
captain-tab <d491405879@gmail.com>
2021-01-11 22:37:48 +0800

    factor row and col

--- src/Col.vue
+++ src/Col.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="col"
-       :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-       :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}"
-  >
-     <div style="border: 1px solid green; height: 100px">
+       :class="colClass"
+       :style="colStyle">
        <slot></slot>
-     </div>
   </div>
 </template>
 
@@ -14,7 +11,7 @@ export default {
   name: "Col",
   data () {
     return {
-      gutter: 0
+      gutter: 0,
     }
   },
   props: {
@@ -24,6 +21,21 @@ export default {
     offset: {
       type: [Number, String],
     },
+  },
+  computed: {
+    colStyle () {
+      return {
+        paddingLeft: this.gutter / 2 + 'px',
+        paddingRight: this.gutter / 2 + 'px'
+      }
+    },
+    colClass () {
+      let { span, offset } = this
+      return [
+          span && `col-${span}`,
+        offset && `offset-${offset}`
+      ]
+    }
   }
 }
 </script>
