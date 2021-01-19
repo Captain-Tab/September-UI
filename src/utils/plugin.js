3080fdeee2b2cecb6894476ff5332a106b73a298
captain-tab <d491405879@gmail.com>
2021-01-19 17:22:32 +0800

    set only a toast in the page

--- src/utils/plugin.js
+++ src/utils/plugin.js
@@ -1,15 +1,24 @@
 import Toast from "../Toast";
 
+let currentToast
+
+// create a new Toast component
+const createToast = ({Vue, message, propsData, onClose}) => {
+    let Constructor = Vue.extend(Toast)
+    let toast = new Constructor({propsData})
+    toast.$slots.default = [message]
+    toast.$mount()
+    toast.$on('close', onClose)
+    document.body.appendChild(toast.$el)
+    return toast
+}
+
+// make sure there is a only Toast component before create a new Toast
 export default {
     install (Vue, options) {
         Vue.prototype.$toast = function (message, toastOption) {
-            let Constructor = Vue.extend(Toast)
-            let toast = new Constructor({
-                propsData: toastOption
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            currentToast ? currentToast.close() : ''
+            currentToast  = createToast({ Vue, message, propsData: toastOption, onClose: ()=> { currentToast = null} })
         }
     }
 }
